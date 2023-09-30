9ee77b98cce08a3cb650d33e26423d91c2e67940
unknown <majidalessio@gmail.com>
2023-10-01 01:42:49 +0200

    navigation

--- src/common-comp/navigation.jsx
+++ src/common-comp/navigation.jsx
@@ -1,8 +1,26 @@
 import {Link} from "react-router-dom";
 
 function Navigation(){
+
+    window.onload = function(){
+        let menuToggle = document.querySelector('.menu-toggle')
+        menuToggle.onclick = function (){
+            menuToggle.classList.toggle('active')
+        }
+    }
+
     return (
-        <Link to="/about" className='prova'>about</Link>
+        <section className="navigation">
+            <div className="menu-toggle"></div>
+            <div className="menu">
+                <ul>
+                    <li><Link to="/" className='nav-item'>Home</Link></li>
+                    <li><Link to="/about" className='nav-item'>about</Link></li>
+                    <li><Link to="/works" className='nav-item'>works</Link></li>
+                    <li><Link to="/contact" className='nav-item'>contact</Link></li>
+                </ul>
+            </div>
+        </section>
     )
 }
 export default Navigation;
