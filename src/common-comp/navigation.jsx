9725c1638712c507181e57c8e693a4f14c38f0f2
unknown <majidalessio@gmail.com>
2023-10-01 18:18:42 +0200

    Navbar fix

--- src/common-comp/navigation.jsx
+++ src/common-comp/navigation.jsx
@@ -3,9 +3,19 @@ import {Link} from "react-router-dom";
 function Navigation(){
 
     window.onload = function(){
+        let menu_option = "'menu'"
+        let menu_other_option = "'x'"
         let menuToggle = document.querySelector('.menu-toggle')
+        let menuToggle_after = window.getComputedStyle(menuToggle, '::before');
         menuToggle.onclick = function (){
             menuToggle.classList.toggle('active')
+            setTimeout(function(){
+                if(menuToggle_after.content === '"menu"'){
+                    menuToggle.style.setProperty('--menu-content', menu_other_option)
+                }else{
+                    menuToggle.style.setProperty('--menu-content', menu_option)
+                }
+            }, 200)
         }
     }
 
