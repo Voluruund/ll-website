4f53000e67196f0bfb0948a590cef615e170f78b
unknown <majidalessio@gmail.com>
2024-02-23 16:09:04 +0100

    proportions home

--- src/utils/math.js
+++ src/utils/math.js
@@ -5,13 +5,13 @@ export function lerp (p1, p2, t) {
 export function generateImpulseX(){
     let random = Math.random()
     // let impulse = random >= .5 ?  6 :  (-6)
-    let impulse = random >= .5 ?  20 :  (-1)
+    let impulse = random >= .5 ?  20 :  (-20)
     return impulse
 }
 
 export function generateImpulseY(){
     let random = Math.random()
     // let impulse = random >= .5 ?  4 :  (-6)
-    let impulse = random >= .5 ?  20 :  (-2.5)
+    let impulse = random >= .5 ?  20 :  (-20)
     return impulse
 }
