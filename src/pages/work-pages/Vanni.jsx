d665178dd0b25342f1b4b9b87c0ac8dd31f98c6e
unknown <majidalessio@gmail.com>
2024-03-08 16:51:33 +0100

    Vanni works

---
+++ src/pages/work-pages/Vanni.jsx
@@ -0,0 +1,61 @@
+export default function Vanni(){
+    return <>
+        <div className="works-desc-wp mt-70">
+            <div>
+                <p className="work-p">Domizia Vanni</p>
+                <p className="work-subtitle mt-20">UX Design / Web Design</p>
+                <p className="work-subtitle mt-70">2023</p>
+            </div>
+            <div>
+                <p className="work-p">This website try to represent the elegance, style and determination combining a strong lettering font and a classical one, black and white layout with the determination of pink, clear structure combine with animations. This is Domizia Vanni.</p>
+            </div>
+        </div>
+        <div className="work-cta-wp">
+            <a className="button-outline button--atlas-outline" id="footer-cta">
+                <span>Visit Website</span>
+                <div className="marqueebutton-outline" aria-hidden="true">
+                    <div className="marquee__inner-outline">
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                        <span>Visit Website</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div className="works-grid" id="vanni">
+            <div className="row">
+                <img src="./img/vanni/first.png" alt="prova" />
+            </div>
+            <div className="row">
+                <img src="./img/vanni/sl.png" alt="prova" />
+            </div>
+            <div className="row">
+                <img src="./img/vanni/sr.png" alt="prova" />
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+                <div className="inner-row">
+                    <img src="./img/vanni/fofirst.png" alt="prova" />
+                    <img src="./img/vanni/fosecond.png" alt="prova" />
+                    <img src="./img/vanni/fothird.png" alt="prova" />
+                </div>
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+                <img src="./img/vanni/fr.png" alt="prova" />
+            </div>
+        </div>
+    </>
+}
